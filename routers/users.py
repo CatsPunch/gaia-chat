f4f2e736439dd38005a04467a1626b127e4ca780
CatsPunch <90175619+CatsPunch@users.noreply.github.com>
2023-07-21 15:44:42 -0700

    Update users.py

--- routers/users.py
+++ routers/users.py
@@ -1,9 +1,25 @@
-from fastapi import APIRouter
-from app import models, schemas
+from fastapi import APIRouter, Depends
+from ..services import users as user_service
+from ..schemas import User, UserInDB, Token
+from ..core.auth import get_current_active_user
 
 router = APIRouter()
 
-@router.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate):
-    # Implement user creation logic here
-    pass
+@router.get("/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = user_service.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = user_service.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
