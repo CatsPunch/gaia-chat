f9603250ed832cebcfa3392970c79602e84545df
CatsPunch <90175619+CatsPunch@users.noreply.github.com>
2023-07-21 15:44:40 -0700

    Update chatbot.py

--- routers/chatbot.py
+++ routers/chatbot.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
-from app import models, schemas
+from fastapi import APIRouter, Depends
+from ..services import chat as chat_service
+from ..schemas import ChatInput, ChatResponse
+from ..core.auth import get_current_active_user
 
 router = APIRouter()
 
-@router.post("/chatbot/", response_model=schemas.ChatbotResponse)
-def chatbot_endpoint(request: schemas.ChatbotRequest):
-    # Implement your chatbot logic here
-    pass
+@router.post("/", response_model=ChatResponse)
+async def chat_endpoint(chat_input: ChatInput, user = Depends(get_current_active_user)):
+    response = await chat_service.chat(user, chat_input.user_input)
+    return response
