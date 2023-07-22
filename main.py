aa2941fc1a5e2f2b72967f76d56967a1293e31dd
CatsPunch <90175619+CatsPunch@users.noreply.github.com>
2023-07-21 22:46:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from fastapi.responses import JSONResponse
-from .routers import router as api_router
+from gaia_chat.routers import router as api_router
 from .core import auth
 
 # Import routers
